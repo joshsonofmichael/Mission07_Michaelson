6390a18f6dfa4fea5e5bc814973b908606c75200
joshsonofmichael <107602793+joshsonofmichael@users.noreply.github.com>
2024-02-24 18:40:28 -0700

    initialized app

---
+++ Mission07_Michaelson/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Mission07_Michaelson.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
